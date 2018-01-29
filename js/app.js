5168bd611a625f794a58aa5de9004bfd6cb4e917
Elzbeth Blanco <elzbethblanco@gmail.com>
2018-01-29 12:44:35 -0600

    updating views
    updating views

--- js/app.js
+++ js/app.js
@@ -1,15 +1,99 @@
-
+//VARIABLES//
+//sections//
+$splashSection = $('#splash');
+$loginSection = $('#login');
+$signupSection = $('#signup');
+$homeSection = $('#home');
+$profileSection = $('#profile');
+$friendsSection = $('#friends');
+$navBar = $('#principal-nav');
+$logout = $('#logout');
+//buttons//
+$logingBtn = $('#login-btn');
+$profileBtn = $('#profile-btn');
+$friendsBtn = $('#friends-btn');
+$logoutBtn = $('#logout-btn');
+//DOCUMENT//
 $(document).ready(function(e){
-$('select').material_select(); //activates selections in modals
-$('.modal').modal();
+  //Nav, Modals and Select//
+  $(".button-collapse").sideNav();
+  $('select').material_select(); //activates selections in modals
+  $('.modal').modal();
+  //Nav, Modals and Select End//
+  //Splash Page//
+  $loginSection.hide();
+  $signupSection.hide();
+  $homeSection.hide();
+  $profileSection.hide();
+  $friendsSection.hide();
+  $navBar.hide();
+  $logout.hide();
+  $splashSection.fadeOut(2000, function() {
+  $loginSection.fadeIn(3000);
+  });
+
+});
+//Splash Page End//
+//FIREBASE//
+// var config = {
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: ""
+// };
+// firebase.initializeApp(config);
+
+//NAVIGATION//
+
+//showing home//
+
+
+$logingBtn.click(function() {
+
+console.log('home');
+
+
 });
 
-var config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
-};
-firebase.initializeApp(config);
+
+//showing profile//
+$profileBtn.click(function() {
+
+console.log('profile');
+
+
+
+});
+
+//showing friends//
+
+$friendsBtn.click(function() {
+
+console.log('friends');
+
+
+
+});
+
+//showing logout//
+$logingBtn.click(function() {
+
+console.log('logout');
+
+
+
+});
+
+//FUNCTIONS//
+
+//new post//
+
+//add friend//
+
+//delete friend//
+
+//like//
+
+//tag friend//
